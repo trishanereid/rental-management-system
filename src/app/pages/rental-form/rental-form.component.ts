ebc5edc7b1b3ff87b8223cda45e3a573cd62967f
trishane <trishanereid@gmail.com>
2024-07-26 17:40:07 +0530

    updated

--- src/app/pages/rental-form/rental-form.component.ts
+++ src/app/pages/rental-form/rental-form.component.ts
@@ -13,31 +13,64 @@ import { HeaderComponent } from "../../header/header.component";
   styleUrl: './rental-form.component.css'
 })
 
-export class RentalFormComponent  implements OnInit{
+export class RentalFormComponent implements OnInit {
+searchRental() {
+  this.http.get('http://localhost:8080/api/v4/search-rent-by-id/'+ this.inputItemId).subscribe(
+    data => {
+      this.rental = data;
+    },
+    error => {
+      alert("Rental creation not successfull")
+    }
+  )
+}
+  deleteRental() {
+    this.http.delete('http://localhost:8080/api/v4/delete-rental/'+ this.inputItemId).subscribe(
+      data => {
+        alert("Rental added Successfully")
+      },
+      error => {
+        alert("Rental creation not successfull")
+      }
+    )
+  }
 
   inputItemId: any;
-  inputRentalDate: Date | any;
+  inputRentalDate: any = '';
   inputReturnDate: Date | any;
   inputDueDate: any;
-  total: number|any;
+  total: number | any;
   constructor(private http: HttpClient) { }
 
+  rental = {
+    rentDate: "",
+    returnDate: "",
+    dueDate: "",
+    totalCost: ""
+  }
+
   ngOnInit(): void {
-    this.total = this.dateDiffrernce(this.inputRentalDate, this.inputReturnDate)*50;
+    // this.total = this.dateDiffrernce(this.inputRentalDate, this.inputReturnDate)*50;
   }
-
-
   addRental() {
-    
+    this.http.post('http://localhost:8080/api/v4/add-rent', this.rental).subscribe(
+      response => {
+        alert("Rental added Successfully")
+      },
+      error => {
+        alert("Rental creation not successfull")
+      }
+    );
+    console.log(this.rental);
   }
 
-  dateDiffrernce(date1: Date, date2: Date): number {
-    // const oneDay = 24 * 60 * 60 * 1000;
-    // const difInTime = date2.getTime() - date1.getTime();
-    this.total = this.dateDiffrernce(this.inputRentalDate, this.inputReturnDate)*50;
-    return 10;
-  }
+  // dateDiffrernce(date1: Date, date2: Date): number {
+  //   const oneDay = 24 * 60 * 60 * 1000;
+  //   const difInTime = date2.getTime() - date1.getTime();
+  //   this.total = this.dateDiffrernce(this.inputRentalDate, this.inputReturnDate)*50;
+  //   return 10;
+  // }
 
-  
+
 
 }
